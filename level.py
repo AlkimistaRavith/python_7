ebf70c3b9551c052ca3c99d7583d397fb53809d0
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-10 23:59:59 +0000

    avance

--- level.py
+++ level.py
@@ -1,13 +1,19 @@
 def choose_level(n_pregunta, p_level):
-    
-    # Construir lógica para escoger el nivel
-    ##################################################
-    pass
-    
-    
-    ##################################################
-    
-    return level
+    if p_level == 2:
+        if n_pregunta <= 2:
+            return "basica"
+        elif n_pregunta <= 4:
+            return "intermedia"
+        else:
+            return "avanzada"
+    elif p_level == 3:
+        if n_pregunta <= 3:
+            return "basica"
+        elif n_pregunta <= 6:
+            return "intermedia"
+        else:
+            return "avanzada"
+
 
 if __name__ == '__main__':
     # verificar resultados
