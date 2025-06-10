cf6cc07869818e116559cd53d87119084825937b
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-10 06:06:14 +0000

    avance tarea

---
+++ preguntas.py
@@ -0,0 +1,60 @@
+preguntas_basicas = {
+    'pregunta_1': {'enunciado':['¿Cuál dios habita en el Monte Olimpo?'],
+    'alternativas': [['Ra', 0], 
+                     ['Zeus', 1], 
+                     ['Hades', 0], 
+                     ['Odin', 0]]},
+    'pregunta_2': {'enunciado':['¿Cuál dios fue faraon por 1000 años?'],
+    'alternativas': [['Odin', 0], 
+                     ['Ra', 1], 
+                     ['Zeus', 0], 
+                     ['Seth', 0]]},
+    
+'pregunta_3': {'enunciado':['¿Qué dios tiene 2 cuervos ayudantes y vive en Asgard?'],
+    'alternativas': [['Zeus', 0], 
+                     ['Odin', 1], 
+                     ['Ra', 0], 
+                     ['Loki', 0]]}
+}
+
+preguntas_intermedias = {
+    'pregunta_1': {'enunciado':['¿A quien le pertenece el martillo de Mjolnir?'],
+    'alternativas': [['Odin', 0], 
+                     ['Thor', 1], 
+                     ['Loki', 0], 
+                     ['Heimdall', 0]]},
+    'pregunta_2': {'enunciado':['En la mitología griega, ¿Quién es el dios del inframundo?'],
+    'alternativas': [['Zeus', 0], 
+                     ['Hades', 1], 
+                     ['Ares', 0], 
+                     ['Poseidon', 0]]},
+    
+'pregunta_3': {'enunciado':['En la mitología egipcia, ¿Quien era la diosa de la maternidad, la magia y la protección?'],
+    'alternativas': [['Bastet', 0], 
+                     ['Isis', 1], 
+                     ['Uadjet', 0], 
+                     ['Ptah', 0]]}
+}
+
+preguntas_avanzadas = {
+    'pregunta_1': {'enunciado':['En la mitología griega, ¿Quien fue Calisto?'],
+    'alternativas': [['Sacerdotiza de Atenea, quien se acostó con Poseidon en el templo de Atenea y fue castigada y transformada en Medusa.', 0], 
+                     ['Cazadora del cortejo de Artemisa, quien fue engañada por Zeus transformado en Artemisa, para acostarse con ella. De castigo fue transformada en osa, y Zeus la convirtió en la constelación de la Osa Mayor.', 1], 
+                     ['Hija de Demeter, raptada por Hades para ser su esposa. Aunque fue devuelta, debe pasar 6 meses en el Inframundo, para cumplir su voto de esposa.]', 0], 
+                     ['Esposa de Anfitrión, quien fue engañada y embarazada por Zeus, dando a luz al semidios Heracles (o Hércules).', 0]]},
+    'pregunta_2': {'enunciado':['Luego de su retiro como faraon, Ra dios del Sol, se enfrenta cada noche a una bestia de la mitología egipcia. ¿Quién es esta bestia?'],
+    'alternativas': [['Khepri, el escarabajo del renacimiento.', 0], 
+                     ['Bennu, el ave fénix de la creación.', 0], 
+                     ['Apofis, una serpiente gigante que representa la oscuridad y el caos cósmico.', 1], 
+                     ['Ammit, una criatura infernal que devoraba los corazones indignos.', 0]]},
+    
+'pregunta_3': {'enunciado':['En la mitología nordica, ¿Cómo se llaman los 2 cuervos de Odin, que representan el pensamiento y la memoria.'],
+    'alternativas': [['Fenrir y Jormungandr', 0], 
+                     ['Hugin y Munin.', 1], 
+                     ['Tanngrisnir y Tanngnjóstr', 0], 
+                     ['Geri y Freki', 0]]}
+}
+
+pool_preguntas = {'basicas': preguntas_basicas,
+                  'intermedias': preguntas_intermedias,
+                  'avanzadas': preguntas_avanzadas}
