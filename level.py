7bb3c08df7eb69f24b708de6f50b78cb0d777725
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-11 03:56:57 +0000

    avance

--- level.py
+++ level.py
@@ -1,23 +1,31 @@
 def choose_level(n_pregunta, p_level):
-    p_totales = p_level * 3
-    if n_pregunta > p_totales or n_pregunta < 1:
-        print(f"El número ingresado de la pregunta no existe. \nPor favor ingresar un número entre 1 y {p_totales}, para {p_level} preguntas por nivel.")
-        return None
+    p_totales = p_level * 3 #Total de preguntas que se obtienen en X preguntas por nivel.
 
-    preguntas = {
-        "Basica": [],
-        "Intermedia": [],
-        "Avanzada": []
-    }
+    if 0 < p_level <= 3: #Validación de preguntas por nivel, solo da resultado con valores 1, 2 o 3.
 
-    for i in range(1, p_level + 1):
-        preguntas["Basica"].append(i)
-        preguntas["Intermedia"].append(i + p_level)
-        preguntas["Avanzada"].append(i + 2 * p_level)
+        if n_pregunta > p_totales or n_pregunta < 1: #Validación del N° de la pregunta, no puede exceder el total de preguntas.
+            print(f"El número ingresado de la pregunta no existe. \n Por favor ingresar un número entre 1 y {p_totales}, para {p_level} preguntas por nivel.")
+            return ""
+        #diccionario para guardar los numeros de las preguntas según su nivel.
+        preguntas = {
+            "Basica": [],
+            "Intermedia": [],
+            "Avanzada": []
+        }
 
-    for nivel, indices in preguntas.items():
-        if n_pregunta in indices:
-            return nivel
+        for i in range(1, p_level + 1):
+            preguntas["Basica"].append(i) #lista las preguntas basicas en los primeros numeros.
+            preguntas["Intermedia"].append(i + p_level) #lista las preguntas intermedias en los numeros medios.
+            preguntas["Avanzada"].append(i + 2 * p_level) #lista las preguntas avanzadas en los últimos numeros.
+
+        for nivel, indices in preguntas.items():
+            #El numero de pregunta solicitado está en una de las lista del diccionario(indices), entrega el resultado correspondiente (nivel).
+            if n_pregunta in indices:
+                return nivel
+            
+    else: #Error si no se ingresa 1, 2 o 3 en el item p_level (preguntas por nivel)
+        print("Error en el ingreso de preguntas por nivel. \n Por favor seleccionar entre 1 y 3 preguntas por nivel.")
+        return ""
 
 
 if __name__ == '__main__':
@@ -26,3 +34,4 @@ if __name__ == '__main__':
     print(choose_level(3, 2)) # intermedias
     print(choose_level(7, 2)) # [avanzadas] Error: la selección (7,2) es incorrecta, ya que para 2 preguntas por nivel, da un total de 6 preguntas, no existe la prgunta 7.
     print(choose_level(4, 3)) # intermedias
+    print(choose_level(4, 5)) # Error: El ingreso no es valido para preguntas por nivel.
--- validador.py
+++ validador.py
@@ -1,20 +1,17 @@
 
 def validate(opciones, eleccion):
-    # Definir validación de eleccion
+    # Mientras no se seleccione una opcion valida, pedirá nuevamente una elección.
     while eleccion not in opciones:
         print(f"Opción no válida, ingrese una de las opciones válidas: {opciones}")
         eleccion = input("Ingrese una opción: ").lower()
     return eleccion
-    
+
+#opciones de validación
+opciones = ["a", "b", "c", "d"]    
 
 
 if __name__ == '__main__':
-    opciones = ["a", "b", "c", "d"]
     eleccion = input('Ingresa una Opción: ').lower()
-    # letras = ['a','b','c','d'] # pueden probar con letras también para verificar su funcionamiento.
-    
+
     resultado = validate(opciones, eleccion)
     print(f"Opción válida ingresada: {resultado}")
-    
-    
-    
